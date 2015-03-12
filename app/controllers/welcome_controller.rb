60916add8024441aeaacb5fe1e882a5c7c8e69f8
LucienMoor <lucien.moor@gmail.com>
2015-03-12 13:09:48 +0000

    login inside the navbar and logout

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,5 +1,6 @@
 class WelcomeController < ApplicationController
   def index
+    @user = User.new
     render 'index'
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,5 @@
 Rails.application.routes.draw do
-  
-  devise_for :users, controllers: { registrations: "users/registrations" }
+  devise_for :users,  controllers: {registrations: "users/registrations"}
   #not sure about this
   #root to: "users#index"
   
