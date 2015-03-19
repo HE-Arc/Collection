fcc4c3ea33c677cc64d931b2889d3e958a2a1900
Gary Marigliano <gmarigliano93@gmail.com>
2015-03-19 14:59:28 +0000

    add sort cd in timeline

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,6 +1,30 @@
 class WelcomeController < ApplicationController
   def index
-    @cds = Cd.all
+    @cds =nil
+
+    sort = params[:sort]
+    if sort.nil? || sort == ''
+      @cds = Cd.all
+    else
+      case sort
+        when 'date'
+          @cds = Cd.all.order('created_at')
+        when 'dateDesc'
+          @cds = Cd.all.order('created_at DESC')
+        when 'name'
+          @cds = Cd.all.order('name')
+        when 'nameDesc'
+          @cds = Cd.all.order('name DESC')
+
+        when 'purchaseDate'
+          @cds = Cd.all.order('purchaseDate')
+        when 'purchaseDateDesc'
+          @cds = Cd.all.order('purchaseDate DESC')
+        else
+          @cds = Cd.all
+      end
+    end
+
     render 'index'
   end
 end
