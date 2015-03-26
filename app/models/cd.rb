a55798618556f02b0764296240136fe38f119472
Gary Marigliano <gmarigliano93@gmail.com>
2015-03-26 13:05:21 +0000

    fix cover image size

--- app/models/cd.rb
+++ app/models/cd.rb
@@ -5,10 +5,10 @@ class Cd < ActiveRecord::Base
   validates :name,
             presence: true
 
-  validates :image, cover: { min_size: 300 }
+  validates :image, cover: {min_size: 300}
 
   has_attached_file :image, styles: {small: "64x64", med: "128x128", large: "256x256"}, :default_url => "cd/:style/missing.png"
 
-  validates_attachment :image, :presence => true,
+  validates_attachment :image,
                        :content_type => {:content_type => ["image/jpg", "image/jpeg", "image/png", "image/gif"]}
 end
