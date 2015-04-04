406d278fa555600f939a5d9a9aa6128a386cce5c
Gary Marigliano <gmarigliano93@gmail.com>
2015-04-04 13:43:39 +0000

    remove size validation for image upload, too much pain for a little bonus

--- app/models/cd.rb
+++ app/models/cd.rb
@@ -3,27 +3,25 @@ class Cd < ActiveRecord::Base
   has_many :tracks, dependent: :destroy
 
   validates_presence_of :name, :artist, :editor, :gender
-  
+
   validates :year, :inclusion => 1900..Date.today.year
 
-  validates :item_collection_id , presence: true
+  validates :item_collection_id, presence: true
   validate :max_purchaseDate
-  
+
   validate :bugDate
 
   def max_purchaseDate
     if purchaseDate
-      errors.add(:purchaseDate, "La date est dépassé.") if purchaseDate > Date.today
+      errors.add(:purchaseDate, "La date d'achat est dépassée.") if purchaseDate > Date.today
     end
   end
-  
+
   def bugDate
     if purchaseDate && year
-      errors.add(:purchaseDate, "La date d'achat est inférieur à l'année de parution") if purchaseDate.year < year
+      errors.add(:purchaseDate, "La date d'achat est inférieure à l'année de parution") if purchaseDate.year < year
     end
   end
-  
-  validates :image, cover: {min_size: 300}
 
   has_attached_file :image, styles: {small: "64x64", med: "128x128", large: "256x256"}, :default_url => "cd/:style/missing.png"
 
